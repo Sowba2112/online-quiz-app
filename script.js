29ca0a2c923159fff31bf703cdced598d4852723
Sowba2112 <sowbarniyas.22ece@kongu.edu>
2026-02-04 14:34:37 +0530

    updated quiz questions and css

--- script.js
+++ script.js
@@ -5,3 +5,54 @@ function checkAnswer(answer) {
         document.getElementById("result").innerText = "Wrong Answer!";
     }
 }
+const quizData = [
+    {
+        question: "What is the capital of India?",
+        a: "Chennai",
+        b: "Delhi",
+        c: "Mumbai",
+        d: "Kolkata",
+        correct: "b"
+    },
+    {
+        question: "Which language is used for web styling?",
+        a: "HTML",
+        b: "Python",
+        c: "CSS",
+        d: "Java",
+        correct: "c"
+    },
+    {
+        question: "Which company developed Java?",
+        a: "Sun Microsystems",
+        b: "Google",
+        c: "Microsoft",
+        d: "Apple",
+        correct: "a"
+    },
+    {
+        question: "What does CPU stand for?",
+        a: "Central Processing Unit",
+        b: "Computer Personal Unit",
+        c: "Central Program Unit",
+        d: "Control Processing Unit",
+        correct: "a"
+    },
+    {
+        question: "Which tag is used for JavaScript?",
+        a: "<js>",
+        b: "<javascript>",
+        c: "<script>",
+        d: "<code>",
+        correct: "c"
+    },
+    {
+        question: "Which symbol is used for comments in CSS?",
+        a: "//",
+        b: "#",
+        c: "/* */",
+        d: "<!-- -->",
+        correct: "c"
+    }
+    ];
+    
